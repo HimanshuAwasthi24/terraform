e60468221d7243d52633412e16a58d56dcaf4297
Himanshu Awasthi <himan.7525@gmail.com>
2024-12-07 05:36:16 +0000

    datasource

---
+++ datasource/datasource.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "existing_resource" {
+  id="existing_vpc_id"
+}
+
+resource "aws_internet_gateway" "resource_to_be_attached" {
+  vpc_id = data.aws_vpc.existing_resource.id #added data.restOfTheVaribale to attach IGW with existing vpc
+  tags = {
+    name="IGW_created_using_terraform"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "bucket-name"
+    key = "backend_file_name.tfstate"
+    region = "ap-south-1"
+  }
+}
